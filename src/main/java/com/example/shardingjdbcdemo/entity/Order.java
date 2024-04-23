fd055c008d3cfcf2d32afa2646f5cbd68c56d150
geek.zhao <jczhao_cs@163.com>
2024-04-24 07:50:02 +0800

    根据订单id进行分布式序列配置（雪花算法）

--- src/main/java/com/example/shardingjdbcdemo/entity/Order.java
+++ src/main/java/com/example/shardingjdbcdemo/entity/Order.java
@@ -16,8 +16,13 @@ import java.math.BigDecimal;
 @TableName("t_order")
 @Data
 public class Order {
-    // @TableId(type = IdType.AUTO)
-     @TableId(type = IdType.ASSIGN_ID) // 分布式ID
+
+    /**
+     * 当配置了shardingsphere-jdbc的分布式序列时，自动使用shardingsphere-jdbc的分布式序列
+     * 当没有配置shardingsphere-jdbc的分布式序列时，自动依赖数据库的主键自增策略
+     */
+    @TableId(type = IdType.AUTO)
+     // @TableId(type = IdType.ASSIGN_ID) // 分布式id（默认值）
     private Long id;
     private String orderNo;
     private Long userId;
